5ffa3895976e6c9a348e8d19991502bdd8a003b6
suminlee <suminpixel@gmail.com>
2020-04-29 02:31:00 +0900

    충돌 & 중력(gravity) 실습

---
+++ Assets/Scripts/TestCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollision : MonoBehaviour
+{
+    // 1. RigitBody (isKinnematic: off)
+    // 2.Collider (isTrigger: off)
+    // 3.상대방에 Collider (isTrigger : off)
+    private void OnCollisionEnter(Collision collision){
+        Debug.Log("Collistion");
+    }
+
+    private void OnTriggetEnter(Collider other){
+        Debug.Log("Trigger");
+    }
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
